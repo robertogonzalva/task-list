e295f35b65ac60b798e6e445127bc18043798e36
robertogonzalva <robertogonzalva@gmail.com>
2022-02-08 23:13:11 +0100

    chore: add PendingTask component inside ToDoList

--- components/ToDoList.jsx
+++ components/ToDoList.jsx
@@ -1,8 +1,12 @@
 import ToDoItem from "./ToDoItem";
+import PendingTasks from "./PendingTasks";
 
-export default function ToDoList({ tasks, toggleTask }) {
+export default function ToDoList({ tasks, toggleTask, pendingTasks }) {
   return (
     <div>
+      <div className="mt-10 mb-3">
+        <PendingTasks pendingTasks={pendingTasks} />
+      </div>
       <ul>
         {tasks.map((task) => (
           <ToDoItem key={task.key} task={task} toggleTask={toggleTask} />
