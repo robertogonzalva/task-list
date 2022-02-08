0a145f119406e6781c404ce05bbadccd3e8f04d7
robertogonzalva <robertogonzalva@gmail.com>
2022-02-09 00:20:51 +0100

    chore: add favicon

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function App() {
   return (
     <>
       <Head>
-        <title>Task list creator 📝</title>
+        <title>Task list creator</title>
         <meta
           name="description"
           content="In this website you will be able to list all task you have"
@@ -67,8 +67,8 @@ export default function App() {
 
       <main className="bg-zinc-100 min-h-screen">
         <section className="container mx-auto px-42">
-          <div class="px-5 py-20 mx-auto">
-            <div class="flex flex-col text-center">
+          <div className="px-5 py-20 mx-auto">
+            <div className="flex flex-col text-center">
               <span className="sm:text-5xl text-4xl font-extrabold text-slate-800">
                 Create your{" "}
                 <h1 className="sm:text-5xl text-4xl font-extrabold leading-normal text-center bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-red-300 inline">
@@ -77,7 +77,7 @@ export default function App() {
               </span>
             </div>
           </div>
-          <div class="flex justify-center max-h-10">
+          <div className="flex justify-center max-h-10">
             <input
               ref={taskRef}
               className="auto-focus shadow-md rounded-md mr-2 w-7/12 border border-slate-300 focus:border-slate-400 focus:ring-2 focus:ring-slate-200 text-base outline-none text-gray-700 py-1 px-3 transition-colors duration-200 ease-in-out"
