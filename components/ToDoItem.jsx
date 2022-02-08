2d69a5897b2fe71c64791facace1784141bb1475
robertogonzalva <robertogonzalva@gmail.com>
2022-02-08 23:13:31 +0100

    chore: add label to description task

--- components/ToDoItem.jsx
+++ components/ToDoItem.jsx
@@ -6,14 +6,14 @@ export default function ToDoItem({ task, toggleTask }) {
   };
 
   return (
-    <li className="font-medium">
+    <li>
       <input
-        className="mr-2 align-middle"
+        className="mr-2 align-middle appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-slate-600 checked:border-slate-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2"
         type="checkbox"
         checked={completed}
         onChange={handleTaskCompleted}
       />
-      {description}
+      <label className="font-semibold text-lg">{description}</label>
     </li>
   );
 }
